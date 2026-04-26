a0d31255b50a004943ae1b71fd70cd9136467a91
cvidal <cvidal@arssenasa.gob.do>
2026-04-25 20:49:07 -0400

    Modificacion docker hub

--- Program.cs
+++ Program.cs
@@ -1,26 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+// IMPORTANTE para Render
+app.Urls.Add("http://0.0.0.0:80");
 
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapGet("/", () => "Hola Mundo desde .NET 🚀");
 
 app.Run();
