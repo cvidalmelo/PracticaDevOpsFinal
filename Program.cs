90d2544e70ff0e0112bf4dfc6c5ab4a7ebd7695b
cvidal <cvidal@arssenasa.gob.do>
2026-04-25 21:48:39 -0400

    Modificacion archivo yalm

--- Program.cs
+++ Program.cs
@@ -5,6 +5,6 @@ var app = builder.Build();
 // IMPORTANTE para Render
 app.Urls.Add("http://0.0.0.0:80");
 
-app.MapGet("/", () => "Hola Mundo desde .NET 🚀");
+app.MapGet("/", () => "Hola Mundo desde .NET");
 
 app.Run();
