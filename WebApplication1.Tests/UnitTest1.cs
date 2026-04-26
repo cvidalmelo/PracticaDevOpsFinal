345624e1fa6a4f80ce2e2fab46e9b14ec7b4a767
cvidal <cvidal@arssenasa.gob.do>
2026-04-25 21:59:17 -0400

    Git Action Configuration

---
+++ WebApplication1.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Xunit;
+
+namespace WebApplication1.Tests;
+
+public class UnitTest1 : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public UnitTest1(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Inicio_DebeRetornar_HolaMundo()
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync("/");
+        var contenido = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Contains("Hola", contenido);
+    }
+}
